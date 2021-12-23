da12a48bb68ecca53276fd88c87984b2c1058396
Isabella Damião <isabella.damiao@wconnect.com.br>
2021-12-23 11:25:07 -0300

    up cors

--- src/main/java/com/crud/agenda/configuration/ConfigurationCors.java
+++ src/main/java/com/crud/agenda/configuration/ConfigurationCors.java
@@ -4,11 +4,12 @@ package com.crud.agenda.configuration;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
 
 @Configuration
 @EnableWebMvc
-public class ConfigurationCors extends WebMvcConfigurerAdapter {
+public class ConfigurationCors implements WebMvcConfigurer {
 	
 		  @Override
 		  public void addCorsMappings(CorsRegistry registry) {
