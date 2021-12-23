24565e77c2316234f28a35337738645505001f3f
Isabella Damião <isabella.damiao@wconnect.com.br>
2021-12-23 11:58:48 -0300

    up cors

--- src/main/java/com/crud/agenda/configuration/ConfigurationCors.java
+++ src/main/java/com/crud/agenda/configuration/ConfigurationCors.java
@@ -3,21 +3,24 @@ package com.crud.agenda.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.Ordered;
-import org.springframework.core.annotation.Order;
+
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 
 @Configuration
-@Order(Ordered.HIGHEST_PRECEDENCE)
-@EnableWebMvc
-public class ConfigurationCors extends WebMvcConfigurerAdapter {
+public class ConfigurationCors {
 
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedOrigins("*");
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+    	
+        return new WebMvcConfigurer() {
+        	
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/agenda/**").allowedOrigins("*");
+            }
+        };
     }
 }
