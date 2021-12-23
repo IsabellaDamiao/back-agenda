b74e1a6974cac456469c4456bb69e489a45cfa71
Isabella Damião <isabella.damiao@wconnect.com.br>
2021-12-23 12:12:26 -0300

    up cors

--- src/main/java/com/crud/agenda/AgendaApplication.java
+++ src/main/java/com/crud/agenda/AgendaApplication.java
@@ -3,10 +3,7 @@ package com.crud.agenda;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
 
 @SpringBootApplication
 public class AgendaApplication implements CommandLineRunner {
@@ -20,20 +17,6 @@ public class AgendaApplication implements CommandLineRunner {
 
 	}
 	
-	@SuppressWarnings("deprecation")
-	@Bean
-    public WebMvcConfigurer corsConfigurer() {
-		
-        return new WebMvcConfigurerAdapter() {
-        	
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/agenda/**").allowedOrigins("http://localhost:4200").allowedMethods("PUT", "DELETE",
-                        "GET", "POST");
-            }
-        };
-    }
-
 }
 
 
--- src/main/java/com/crud/agenda/configuration/ConfigurationCors.java
+++ src/main/java/com/crud/agenda/configuration/ConfigurationCors.java
@@ -1,10 +1,20 @@
 package com.crud.agenda.configuration;
 
-import org.springframework.context.annotation.Configuration;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 
-@Configuration
-public class ConfigurationCors {
+@SuppressWarnings("deprecation")
+@RestController
+@CrossOrigin
+public class ConfigurationCors extends WebMvcConfigurerAdapter {
 
-    
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**");
+    }
 }
+
